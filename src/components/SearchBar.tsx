a2e1287509cb147141210006eb7c076f5def1c49
Specter-009 <xvr09200@gmail.com>
2026-04-03 17:00:10 +0800

    Added searchbar feature to Tab 3

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IonSearchbar } from '@ionic/react';
+
+function Example() {
+  return (
+    <>
+      <IonSearchbar animated={true} placeholder="Animated"></IonSearchbar>
+    </>
+  );
+}
+export default Example;
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import SearchBar from '../components/SearchBar';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +7,17 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Specter-009</IonTitle>
+          <IonTitle>Search Page</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large"></IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <SearchBar />
+       
       </IonContent>
     </IonPage>
   );
